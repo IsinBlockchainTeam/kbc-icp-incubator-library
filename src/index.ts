7406ba949bfe756f4cc3c82bab5b58e1bc913ba1
Agnola Tommaso <tommaso.agnola@supsi.ch>
2023-08-21 13:37:01 +0200

    feat 🦄:  Supply chain graph generation

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { Material } from './entities/Material';
+export { Trade } from './entities/Trade';
+export { Transformation } from './entities/Transformation';
+
+export { GraphService } from './services/GraphService';
