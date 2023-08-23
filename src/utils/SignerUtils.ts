1b43da5b012200b5af3fdf64d5ee71428c9c26e8
Luca Giussani <luca.giussani@supsi.ch>
2023-08-23 18:00:24 +0200

    get signer from private key with only a network name instead of provider

--- src/utils/SignerUtils.ts
+++ src/utils/SignerUtils.ts
@@ -1,8 +1,9 @@
-import { ExternalProvider, JsonRpcProvider } from '@ethersproject/providers';
+import { ExternalProvider } from '@ethersproject/providers';
 import { ethers, Signer, Wallet } from 'ethers';
 
 export class SignerUtils {
-    static getSignerFromPrivateKey(privateKey: string, provider: JsonRpcProvider): Signer {
+    static getSignerFromPrivateKey(privateKey: string, network: string): Signer {
+        const provider = new ethers.providers.JsonRpcProvider(network);
         return new Wallet(privateKey, provider);
     }
 
