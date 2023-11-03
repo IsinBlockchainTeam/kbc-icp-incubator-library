f9306c2ec838046cb80d6664854096dcb7e750f2
mattia.delloca <mattia.delloca@supsi.ch>
2023-11-03 17:19:45 +0100

    :wip::  Create EscrowManager contract

--- blockchain/contracts/EscrowManager.sol
+++ blockchain/contracts/EscrowManager.sol
@@ -2,30 +2,168 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/utils/escrow/RefundEscrow.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 
 import "hardhat/console.sol";
 
-contract EscrowManager is RefundEscrow, AccessControl {
+contract EscrowManager is AccessControl {
+    using Address for address payable;
+    using Counters for Counters.Counter;
+    Counters.Counter private _counter;
+
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
-    uint256 private _deployedAt;
-    uint256 private _escrowDuration;
+
+    enum State {
+        Active,
+        Refunding,
+        Closed
+    }
+
+    event EscrowRegistered(uint256 indexed id, address payable beneficiary, address payable payer);
+    event Deposited(uint256 id, uint256 amount);
+    event Closed(uint256 id);
+    event RefundEnabled(uint256 id);
+    event Withdrawn(uint256 id, uint256 amount);
+    event Refunded(uint256 id, uint256 amount);
+
 
     modifier onlyAdmin() {
-        require(hasRole(ADMIN_ROLE, _msgSender()), "Caller is not the admin");
+        require(hasRole(ADMIN_ROLE, _msgSender()), "EscrowManager: caller is not the admin");
         _;
     }
 
-    constructor(address payable beneficiary_, uint256 escrowDuration_) RefundEscrow(beneficiary_) {
-        _deployedAt = block.timestamp;
-        _escrowDuration = escrowDuration_;
+    modifier onlyBeneficiary(uint256 id) {
+        require(escrows[id].beneficiary == _msgSender(), "EscrowManager: caller is not the beneficiary");
+        _;
     }
 
-    function hasExpired() public view returns (bool) {
-        return block.timestamp >= _deployedAt + _escrowDuration;
+    modifier onlyPayer(uint256 id) {
+        require(escrows[id].payer == _msgSender(), "EscrowManager: caller is not the payer");
+        _;
     }
 
-    function withdrawalAllowed2(address payer) public view returns (bool) {
-        return hasExpired() || super.withdrawalAllowed(payer);
+    modifier onlyExistingEscrows(uint256 id) {
+        require(escrowExists(id), "EscrowManager: escrow does not exist");
+        _;
+    }
+
+    struct Escrow {
+        State state;
+        address payable beneficiary;
+        address payable payer;
+        uint256 depositAmount;
+        uint256 deployedAt;
+        uint256 duration;
+    }
+
+    // escrow id => escrow
+    mapping(uint256 => Escrow) private escrows;
+
+    constructor(address[] memory admins) {
+        _setupRole(ADMIN_ROLE, msg.sender);
+        _setRoleAdmin(ADMIN_ROLE, ADMIN_ROLE);
+
+        for (uint256 i = 0; i < admins.length; ++i) {
+            grantRole(ADMIN_ROLE, admins[i]);
+        }
+    }
+    function _registerEscrow(uint256 id, address payable _beneficiary, address payable _payer, uint256 _duration) private {
+        Escrow memory newEscrow = Escrow({
+            state: State.Active,
+            beneficiary: _beneficiary,
+            payer: _payer,
+            depositAmount: 0,
+            deployedAt: block.timestamp,
+            duration: _duration
+        });
+
+        escrows[id] = newEscrow;
+        emit EscrowRegistered(id, _beneficiary, _payer);
+    }
+
+    function registerEscrow(address payable _beneficiary, address payable _payer, uint256 _duration) public returns (uint256) {
+        require(_beneficiary != address(0), "EscrowManager: beneficiary is the zero address");
+        require(_payer != address(0), "EscrowManager: payer is the zero address");
+        uint256 id = _counter.current();
+        _counter.increment();
+        _registerEscrow(id, _beneficiary, _payer, _duration);
+        return id;
+    }
+
+    function getEscrow(uint256 id) public view returns (Escrow memory) {
+        return escrows[id];
+    }
+
+    function getState(uint256 id) public view returns (State) {
+        return escrows[id].state;
+    }
+
+    function getBeneficiary(uint256 id) public view returns (address payable) {
+        return escrows[id].beneficiary;
+    }
+
+    function getPayer(uint256 id) public view returns (address payable) {
+        return escrows[id].payer;
+    }
+
+    function getDepositAmount(uint256 id) public view returns (uint256) {
+        return escrows[id].depositAmount;
+    }
+
+    function hasExpired(uint256 id) public view returns (bool) {
+        return block.timestamp >= escrows[id].deployedAt + escrows[id].duration;
+    }
+
+    function withdrawalAllowed(uint256 id) public view returns (bool) {
+        return escrows[id].state == State.Closed;
+    }
+
+    function refundAllowed(uint256 id) public view returns (bool) {
+        return escrows[id].state == State.Refunding || hasExpired(id);
+    }
+
+    function escrowExists(uint256 id) public view returns (bool) {
+        return (
+            escrows[id].beneficiary != address(0) ||
+            escrows[id].payer != address(0) ||
+            escrows[id].depositAmount != 0 ||
+            escrows[id].deployedAt != 0 ||
+            escrows[id].duration != 0
+        );
+    }
+
+    function deposit(uint256 id) public payable onlyExistingEscrows(id) onlyPayer(id) {
+        require(escrows[id].state == State.Active, "EscrowManager: can only deposit while active");
+        escrows[id].depositAmount += msg.value;
+        emit Deposited(id, msg.value);
+    }
+
+    function withdraw(uint256 id) public onlyBeneficiary(id) onlyExistingEscrows(id) {
+        require(escrows[id].state == State.Closed, "EscrowManager: can only withdraw while closed");
+        uint256 payment = escrows[id].depositAmount;
+        escrows[id].depositAmount = 0;
+        escrows[id].beneficiary.sendValue(payment);
+        emit Withdrawn(id, payment);
+    }
+
+    function refund(uint256 id) public onlyPayer(id) onlyExistingEscrows(id) {
+        require(refundAllowed(id), "EscrowManager: can only refund when allowed");
+        uint256 payment = escrows[id].depositAmount;
+        escrows[id].depositAmount = 0;
+        escrows[id].payer.sendValue(payment);
+        emit Refunded(id, payment);
+    }
+
+    function close(uint256 id) public onlyAdmin onlyExistingEscrows(id) {
+        require(escrows[id].state == State.Active, "EscrowManager: can only close while active");
+        escrows[id].state = State.Closed;
+        emit Closed(id);
+    }
+
+    function enableRefund(uint256 id) public onlyAdmin onlyExistingEscrows(id) {
+        require(escrows[id].state == State.Active, "EscrowManager: can only enable refunds while active");
+        escrows[id].state = State.Refunding;
+        emit RefundEnabled(id);
     }
 }
--- blockchain/test/EscrowManager.test.ts
+++ blockchain/test/EscrowManager.test.ts
@@ -5,45 +5,226 @@ import {expect} from "chai";
 
 describe("EscrowManager", () => {
     let escrowManagerContract: Contract;
-    let owner: SignerWithAddress, payer: SignerWithAddress, beneficiary: SignerWithAddress;
+    let admin: SignerWithAddress, payer: SignerWithAddress, beneficiary: SignerWithAddress;
+    const duration = 60 * 60 * 24 * 30; // 30 days
+    const depositAmount = ethers.utils.parseEther('100');
 
     beforeEach(async () => {
-        [owner, payer, beneficiary] = await ethers.getSigners();
+        [admin, payer, beneficiary] = await ethers.getSigners();
 
-        const duration = 60 * 60 * 24 * 30; // 30 days
         const EscrowManager = await ethers.getContractFactory('EscrowManager');
-        escrowManagerContract = await EscrowManager.deploy(beneficiary.address, duration);
+        escrowManagerContract = await EscrowManager.deploy([admin.address]);
         await escrowManagerContract.deployed();
     });
 
-    describe("RefundEscrow parent contract", () => {
-        it("should return correct beneficiary", async () => {
-            expect(await escrowManagerContract.beneficiary()).to.equal(beneficiary.address);
+    describe("Escrow registration", () => {
+        it("should register escrow", async () => {
+            const tx = await escrowManagerContract.connect(payer).registerEscrow(beneficiary.address, payer.address, duration);
+            const id = await tx.wait().then((tx: any) => tx.events[0].args.id);
+
+            expect(id).to.equal(0);
+            await expect(tx).to.emit(escrowManagerContract, 'EscrowRegistered').withArgs(id, beneficiary.address, payer.address);
         });
 
+        it("should increment id when registering escrows", async () => {
+            let tx = await escrowManagerContract.connect(payer).registerEscrow(beneficiary.address, payer.address, duration);
+            const firstId = await tx.wait().then((tx: any) => tx.events[0].args.id);
+            expect(firstId).to.equal(0);
+
+            tx = await escrowManagerContract.connect(beneficiary).registerEscrow(payer.address, beneficiary.address, duration);
+            const secondId = await tx.wait().then((tx: any) => tx.events[0].args.id);
+            expect(secondId).to.equal(1);
+        });
+
+        it("should retrieve escrow correctly", async () => {
+            let tx = await escrowManagerContract.connect(payer).registerEscrow(beneficiary.address, payer.address, duration);
+            const id = await tx.wait().then((tx: any) => tx.events[0].args.id);
+            expect(id).to.equal(0);
+
+            const escrow = await escrowManagerContract.getEscrow(id);
+            expect(escrow.state).to.equal(0);
+            expect(escrow.beneficiary).to.equal(beneficiary.address);
+            expect(escrow.payer).to.equal(payer.address);
+            expect(escrow.depositAmount).to.equal(0);
+            expect(escrow.duration).to.equal(duration);
+        });
+
+        it("should fail escrow registration if beneficiary is zero address", async () => {
+            await expect(escrowManagerContract.connect(payer).registerEscrow(ethers.constants.AddressZero, payer.address, duration)).to.be.revertedWith("EscrowManager: beneficiary is the zero address");
+        });
+
+        it("should fail escrow registration if payer is zero address", async () => {
+            await expect(escrowManagerContract.connect(payer).registerEscrow(beneficiary.address, ethers.constants.AddressZero, duration)).to.be.revertedWith("EscrowManager: payer is the zero address");
+        });
+    });
+
+    describe("Close", () => {
+        it("should close escrow", async () => {
+            let tx = await escrowManagerContract.connect(admin).registerEscrow(beneficiary.address, payer.address, duration);
+            const id = await tx.wait().then((tx: any) => tx.events[0].args.id);
+
+            tx = await escrowManagerContract.connect(admin).close(id);
+            await expect(tx).to.emit(escrowManagerContract, 'Closed').withArgs(id);
+            expect(await escrowManagerContract.getState(id)).to.equal(2);
+        });
+
+        it("should fail closing escrow if caller is not admin", async () => {
+            let tx = await escrowManagerContract.connect(admin).registerEscrow(beneficiary.address, payer.address, duration);
+            const id = await tx.wait().then((tx: any) => tx.events[0].args.id);
+
+            await expect(escrowManagerContract.connect(payer).close(id)).to.be.revertedWith("EscrowManager: caller is not the admin");
+        });
+
+        it("should fail closing escrow if it does not exist", async () => {
+            const nonExistingId = 10;
+            await expect(escrowManagerContract.connect(admin).close(nonExistingId)).to.be.revertedWith("EscrowManager: escrow does not exist");
+        });
+
+        it("should fail closing escrow if it is not in 'Active' state", async () => {
+            let tx = await escrowManagerContract.connect(admin).registerEscrow(beneficiary.address, payer.address, duration);
+            const id = await tx.wait().then((tx: any) => tx.events[0].args.id);
+
+            await escrowManagerContract.connect(admin).close(id)
+            await expect(escrowManagerContract.connect(admin).close(id)).to.be.revertedWith("EscrowManager: can only close while active");
+        });
     });
 
     describe("Deposit", () => {
         it("should deposit funds", async () => {
-            const depositAmount = 100;
-            const tx = await escrowManagerContract.deposit(payer.address, { value: depositAmount });
+            let tx = await escrowManagerContract.connect(payer).registerEscrow(beneficiary.address, payer.address, duration);
+            const id = await tx.wait().then((tx: any) => tx.events[0].args.id);
 
-            await expect(tx).to.emit(escrowManagerContract, 'Deposited').withArgs(payer.address, depositAmount);
-            expect(await escrowManagerContract.depositsOf(payer.address)).to.equal(depositAmount);
+            tx = await escrowManagerContract.connect(payer).deposit(id, { value: depositAmount });
+
+            await expect(tx).to.emit(escrowManagerContract, 'Deposited').withArgs(id, depositAmount);
+            expect(await escrowManagerContract.getDepositAmount(id)).to.equal(depositAmount);
+        });
+
+        it("should fail deposit if escrow does not exist", async () => {
+            const nonExistentId = 10;
+
+            await expect(escrowManagerContract.connect(payer).deposit(nonExistentId, { value: depositAmount })).to.be.revertedWith("EscrowManager: escrow does not exist");
+        });
+
+        it("should fail deposit if escrow is not in 'Active' state", async () => {
+            let tx = await escrowManagerContract.connect(payer).registerEscrow(beneficiary.address, payer.address, duration);
+            const id = await tx.wait().then((tx: any) => tx.events[0].args.id);
+
+            await escrowManagerContract.connect(payer).deposit(id, { value: depositAmount });
+            await escrowManagerContract.connect(admin).close(id);
+
+            await expect(escrowManagerContract.connect(payer).deposit(id, { value: depositAmount })).to.be.revertedWith("EscrowManager: can only deposit while active");
         });
     });
 
-    describe("Withdrawal", () => {
+    describe("Refund enabling", () => {
+        it("should enable refund", async () => {
+            let tx = await escrowManagerContract.connect(admin).registerEscrow(beneficiary.address, payer.address, duration);
+            const id = await tx.wait().then((tx: any) => tx.events[0].args.id);
+
+            tx = await escrowManagerContract.connect(admin).enableRefund(id);
+            await expect(tx).to.emit(escrowManagerContract, 'RefundEnabled').withArgs(id);
+        });
+
+        it("should fail enabling refund if caller is not admin", async () => {
+            let tx = await escrowManagerContract.connect(payer).registerEscrow(beneficiary.address, payer.address, duration);
+            const id = await tx.wait().then((tx: any) => tx.events[0].args.id);
+
+            await expect(escrowManagerContract.connect(payer).enableRefund(id)).to.be.revertedWith("EscrowManager: caller is not the admin");
+        });
+
+        it("should fail enabling refunds if escrow does not exist", async () => {
+            const nonExistentId = 10;
+
+            await expect(escrowManagerContract.connect(admin).enableRefund(nonExistentId)).to.be.revertedWith("EscrowManager: escrow does not exist");
+        });
+
+        it("should fail enabling refunds if escrow is not in 'Active' state", async () => {
+            let tx = await escrowManagerContract.connect(payer).registerEscrow(beneficiary.address, payer.address, duration);
+            const id = await tx.wait().then((tx: any) => tx.events[0].args.id);
+
+            await escrowManagerContract.connect(admin).close(id);
+
+            await expect(escrowManagerContract.connect(admin).enableRefund(id)).to.be.revertedWith("EscrowManager: can only enable refunds while active");
+        });
+    });
+
+    describe("Refund", () => {
         it("hasExpired should return false if contract has not expired yet", async () => {
-            expect(await escrowManagerContract.hasExpired()).to.false;
+            let tx = await escrowManagerContract.connect(payer).registerEscrow(beneficiary.address, payer.address, duration);
+            const id = await tx.wait().then((tx: any) => tx.events[0].args.id);
+
+            expect(await escrowManagerContract.connect(admin).hasExpired(id)).to.false;
         });
 
-        it("withdrawalAllowed should return true if state is 'Closed' and hasExpired is false", async () => {
-            await escrowManagerContract.close();
+        it("refundAllowed should return true if state is 'Refunding'", async () => {
+            let tx = await escrowManagerContract.connect(payer).registerEscrow(beneficiary.address, payer.address, duration);
+            const id = await tx.wait().then((tx: any) => tx.events[0].args.id);
+            tx = await escrowManagerContract.connect(admin).enableRefund(id);
 
-            expect(await escrowManagerContract.hasExpired()).to.false;
-            expect(await escrowManagerContract.withdrawalAllowed2()).to.true;
+            await expect(tx).to.emit(escrowManagerContract, 'RefundEnabled').withArgs(id);
+            expect(await escrowManagerContract.hasExpired(id)).to.false;
+            expect(await escrowManagerContract.getState(id)).to.equal(1);
+            expect(await escrowManagerContract.refundAllowed(id)).to.true;
+        });
+
+
+        it("should refund funds", async () => {
+            let tx = await escrowManagerContract.connect(payer).registerEscrow(beneficiary.address, payer.address, duration);
+            const id = await tx.wait().then((tx: any) => tx.events[0].args.id);
+            await escrowManagerContract.connect(payer).deposit(id, { value: depositAmount });
+            const initialBalance = await payer.getBalance();
+
+            await escrowManagerContract.connect(admin).enableRefund(id);
+            tx = await escrowManagerContract.connect(payer).refund(id);
+            await tx.wait();
+            expect(tx).emit(escrowManagerContract, 'Refunded').withArgs(id, depositAmount);
+
+            expect(await payer.getBalance()).to.be.greaterThan(initialBalance);
+        });
+
+
+    });
+
+    describe("Withdraw", () => {
+        it("should withdraw funds", async () => {
+            let tx = await escrowManagerContract.connect(payer).registerEscrow(beneficiary.address, payer.address, duration);
+            const id = await tx.wait().then((tx: any) => tx.events[0].args.id);
+            await escrowManagerContract.connect(payer).deposit(id, { value: depositAmount });
+            const initialBalance = await beneficiary.getBalance();
+
+            await escrowManagerContract.connect(admin).close(id);
+            tx = await escrowManagerContract.connect(beneficiary).withdraw(id);
+            await tx.wait();
+            expect(tx).emit(escrowManagerContract, 'Withdrawn').withArgs(id, depositAmount);
+
+            expect(await beneficiary.getBalance()).to.be.greaterThan(initialBalance);
+        });
+
+        it("should fail withdrawing funds if not beneficiary", async () => {
+            let tx = await escrowManagerContract.connect(payer).registerEscrow(beneficiary.address, payer.address, duration);
+            const id = await tx.wait().then((tx: any) => tx.events[0].args.id);
+
+            await expect(escrowManagerContract.connect(payer).withdraw(id)).to.be.revertedWith("EscrowManager: caller is not the beneficiary");
+        });
+
+        it("should fail withdrawing funds if escrow is not in 'Closed' state", async () => {
+            let tx = await escrowManagerContract.connect(payer).registerEscrow(beneficiary.address, payer.address, duration);
+            const id = await tx.wait().then((tx: any) => tx.events[0].args.id);
+
+            await expect(escrowManagerContract.connect(beneficiary).withdraw(id)).to.be.revertedWith("EscrowManager: can only withdraw while closed");
         });
     });
 
+
+    /*
+
+    describe("Roles", () => {
+        it("should return correct beneficiary", async () => {
+            const duration = 60 * 60 * 24 * 30; // 30 days
+            const tx = await escrowManagerContract.connect(payer).registerEscrow(beneficiary.address, payer.address, duration);
+            expect(await escrowManagerContract.beneficiary()).to.equal(beneficiary.address);
+        });
+    });
+*/
 });
