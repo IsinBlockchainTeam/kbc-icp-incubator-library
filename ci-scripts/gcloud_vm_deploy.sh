4ed6a75f2dfd77ddc4814bc0e5cf175eed041617
Luca Giussani <lucagiussani.lg@gmail.com>
2023-11-27 11:03:26 +0100

    ci 🚀: try to fix blockchain deploy stage #64

--- ci-scripts/gcloud_vm_deploy.sh
+++ ci-scripts/gcloud_vm_deploy.sh
@@ -6,6 +6,8 @@ echo "--------- GCP: STOP CONTAINERS ---------"
 docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:$PWD:shared" -w="$PWD" docker/compose:debian-1.29.2 -f docker-compose-prod.yml down
 echo "--------- GCP: LOGIN ---------"
 docker login gitlab-core.supsi.ch:5050 -u $GCLOUD_PULL_USERNAME -p $GCLOUD_PULL_PASSWORD
+echo "--------- GCP: PULL IMAGES ---------"
+docker pull gitlab-core.supsi.ch:5050/dti-isin/giuliano.gremlich/blockchain/one-lib-to-rule-them-all/coffe-trading-management/blockchain-node
 echo "--------- GCP: CLEAR IMAGES AND VOLUMES ---------"
 docker system prune -f --volumes
 echo "--------- GCP: START CONTAINERS ---------"
