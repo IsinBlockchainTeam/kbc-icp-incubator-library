943692e858b7e9229d0fef6f11e4c8625abe810b
Luca Giussani <lucagiussani.lg@gmail.com>
2023-11-30 12:32:14 +0100

    feat 🦄: add global network creation #64

--- ci-scripts/gcloud_vm_deploy.sh
+++ ci-scripts/gcloud_vm_deploy.sh
@@ -10,10 +10,12 @@ docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$SCRIPT_DIR:$SC
     docker/compose:debian-1.29.2 -f docker-compose-prod.yml --env-file .env down
 echo "--------- GCP: LOGIN ---------"
 docker login gitlab-core.supsi.ch:5050 -u $GCLOUD_PULL_USERNAME -p $GCLOUD_PULL_PASSWORD
-echo "--------- GCP: PULL IMAGES ---------"
-docker pull gitlab-core.supsi.ch:5050/dti-isin/giuliano.gremlich/blockchain/one-lib-to-rule-them-all/coffe-trading-management/blockchain-node
 echo "--------- GCP: CLEAR IMAGES AND VOLUMES ---------"
 docker system prune -f --volumes
+echo "--------- GCP: SETTING UP GLOBAL NETWORK ---------"
+docker network inspect bc-coffee-net >/dev/null 2>&1 || docker network create bc-coffee-net
+echo "--------- GCP: PULL IMAGES ---------"
+docker pull gitlab-core.supsi.ch:5050/dti-isin/giuliano.gremlich/blockchain/one-lib-to-rule-them-all/coffe-trading-management/blockchain-node
 echo "--------- GCP: START CONTAINERS ---------"
 docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$SCRIPT_DIR:$SCRIPT_DIR:shared" -w="$SCRIPT_DIR" \
      docker/compose:debian-1.29.2 -f docker-compose-prod.yml --env-file .env up -d --build
