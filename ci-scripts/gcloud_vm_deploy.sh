6a1e78be46b896502902d359e9558af3e0161de9
Luca Giussani <lucagiussani.lg@gmail.com>
2023-12-19 15:36:59 +0100

    ci 🚀: manage docker compose directly without using a dedicated image

--- ci-scripts/gcloud_vm_deploy.sh
+++ ci-scripts/gcloud_vm_deploy.sh
@@ -6,8 +6,7 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
 echo "--------- GCP: STOP CONTAINERS ---------"
 cd $SCRIPT_DIR
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$SCRIPT_DIR:$SCRIPT_DIR:shared" -w="$SCRIPT_DIR" \
-    docker/compose:debian-1.29.2 -f docker-compose-prod.yml --env-file .env down
+docker compose -f docker-compose-prod.yml --env-file .env down
 echo "--------- GCP: LOGIN ---------"
 docker login gitlab-core.supsi.ch:5050 -u $GCLOUD_PULL_USERNAME -p $GCLOUD_PULL_PASSWORD
 echo "--------- GCP: CLEAR IMAGES AND VOLUMES ---------"
@@ -15,7 +14,6 @@ docker system prune -f --volumes
 echo "--------- GCP: SETTING UP GLOBAL NETWORK ---------"
 docker network inspect bc-coffee-net >/dev/null 2>&1 || docker network create bc-coffee-net
 echo "--------- GCP: PULL IMAGES ---------"
-docker pull gitlab-core.supsi.ch:5050/dti-isin/giuliano.gremlich/blockchain/one-lib-to-rule-them-all/coffe-trading-management/blockchain-node
+docker compose -f docker-compose-prod.yml --env-file .env pull
 echo "--------- GCP: START CONTAINERS ---------"
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$SCRIPT_DIR:$SCRIPT_DIR:shared" -w="$SCRIPT_DIR" \
-     docker/compose:debian-1.29.2 -f docker-compose-prod.yml --env-file .env up -d --build
+docker compose -f docker-compose-prod.yml --env-file .env up -d --build
