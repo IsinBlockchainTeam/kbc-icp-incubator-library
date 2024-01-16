1553dc8c153f2a7db8280a5bc0f2b4fdc744ff39
mattia.delloca <mattia.delloca@supsi.ch>
2024-01-16 11:57:13 +0100

    feat 🦄: add ProductCategoryManager contract

---
+++ blockchain/contracts/ProductCategoryManager.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract ProductCategoryManager is AccessControl {
+    using Counters for Counters.Counter;
+
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+
+    modifier onlyAdmin() {
+        require(hasRole(ADMIN_ROLE, _msgSender()), "ProductCategoryManager: Caller is not the admin");
+        _;
+    }
+
+    event ProductCategoryRegistered(uint256 indexed id, string name, uint8 quality);
+
+    struct ProductCategory {
+        uint256 id;
+        string name;
+        uint8 quality;
+        bool exists;
+    }
+
+    Counters.Counter private _counter;
+    mapping(uint256 => ProductCategory) private productCategories;
+
+    constructor() {
+        _setupRole(ADMIN_ROLE, _msgSender());
+        _setRoleAdmin(ADMIN_ROLE, ADMIN_ROLE);
+        grantRole(ADMIN_ROLE, _msgSender());
+    }
+
+    function getProductCategoriesCounter() public view returns (uint256) {
+        return _counter.current();
+    }
+
+    function getProductCategory(uint256 id) public view returns (ProductCategory memory) {
+        return productCategories[id];
+    }
+
+    function registerProductCategory(string memory name, uint8 quality) public {
+        uint256 productCategoryId = _counter.current() + 1;
+        _counter.increment();
+        productCategories[productCategoryId] = ProductCategory(productCategoryId, name, quality, true);
+        emit ProductCategoryRegistered(productCategoryId, name, quality);
+    }
+
+    // ROLES
+    function addAdmin(address admin) public onlyAdmin {
+        grantRole(ADMIN_ROLE, admin);
+    }
+
+    function removeAdmin(address admin) public onlyAdmin {
+        revokeRole(ADMIN_ROLE, admin);
+    }
+}
---
+++ blockchain/test/ProductCategoryManager.test.ts
@@ -0,0 +1,59 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { BigNumber, Contract } from 'ethers';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import { ethers } from 'hardhat';
+import { expect } from 'chai';
+
+describe('ProductCategoryManager', () => {
+    let productCategoryManagerContract: Contract;
+    let admin: SignerWithAddress, other: SignerWithAddress;
+
+    beforeEach(async () => {
+        [admin, other] = await ethers.getSigners();
+
+        const ProductCategoryManager = await ethers.getContractFactory('ProductCategoryManager');
+        productCategoryManagerContract = await ProductCategoryManager.deploy();
+        await productCategoryManagerContract.deployed();
+    });
+
+    describe('Registration', () => {
+        it('should register two ProductCategories', async () => {
+            const previousProductCategoryCounter = await productCategoryManagerContract.getProductCategoriesCounter();
+            expect(previousProductCategoryCounter).to.be.equal(0);
+            const tx = await productCategoryManagerContract.registerProductCategory('testProductCategory', 85);
+            await tx.wait();
+
+            let currentProductCategoryCounter = await productCategoryManagerContract.getProductCategoriesCounter();
+            expect(currentProductCategoryCounter).to.be.equal(1);
+
+            const registeredProductCategory = await productCategoryManagerContract.getProductCategory(1);
+            expect(registeredProductCategory[0]).to.be.equal(BigNumber.from(1));
+            expect(registeredProductCategory[1]).to.be.equal('testProductCategory');
+            expect(registeredProductCategory[2]).to.be.equal(BigNumber.from(85));
+            expect(registeredProductCategory[3]).to.be.equal(true);
+
+            expect(tx).to.emit(productCategoryManagerContract, 'ProductCategoryRegistered').withArgs(registeredProductCategory.id, registeredProductCategory.name, registeredProductCategory.quality);
+
+            const tx2 = await productCategoryManagerContract.registerProductCategory('testProductCategory2', 90);
+            await tx2.wait();
+            currentProductCategoryCounter = await productCategoryManagerContract.getProductCategoriesCounter();
+            expect(currentProductCategoryCounter).to.be.equal(2);
+            const registeredProductCategory2 = await productCategoryManagerContract.getProductCategory(2);
+
+            expect(tx2).to.emit(productCategoryManagerContract, 'ProductCategoryRegistered').withArgs(registeredProductCategory2.id, registeredProductCategory2.name, registeredProductCategory2.quality);
+        });
+    });
+
+    describe('Roles', () => {
+        it('should add and remove admin roles', async () => {
+            await productCategoryManagerContract.connect(admin).addAdmin(other.address);
+            expect(await productCategoryManagerContract.hasRole(await productCategoryManagerContract.ADMIN_ROLE(), other.address)).to.be.true;
+            await productCategoryManagerContract.connect(admin).removeAdmin(other.address);
+            expect(await productCategoryManagerContract.hasRole(await productCategoryManagerContract.ADMIN_ROLE(), other.address)).to.be.false;
+        });
+
+        it('should fail to add and remove admin roles if the caller is not an admin', async () => {
+            await expect(productCategoryManagerContract.connect(other).addAdmin(admin.address)).to.be.revertedWith('ProductCategoryManager: Caller is not the admin');
+        });
+    });
+});
