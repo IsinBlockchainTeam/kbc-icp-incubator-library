f1ff5a25bd04565444dfe8de30c20db17ea2eb9f
Luca Giussani <lucagiussani.lg@gmail.com>
2024-02-09 11:08:01 +0100

    refactor ⚙: update google vm instance name #130

--- ci-scripts/gcloud_deploy.sh
+++ ci-scripts/gcloud_deploy.sh
@@ -4,10 +4,10 @@ PROJECT_FOLDER='blockchain_node'
 
 echo "--------- COFFEE TRADING BLOCKCHAIN DEPLOY (Deployer Container) ---------"
 gcloud auth activate-service-account ci-cd-pipeline@coffe-trading.iam.gserviceaccount.com --key-file=gcloud_auth.json --project=coffe-trading
-gcloud config set compute/zone "europe-west6-a"
+gcloud config set compute/zone "us-central1-a"
 gcloud config set project coffe-trading
-#gcloud compute instances start coffe-trading-instance
-gcloud compute ssh ci-cd-pipeline@coffe-trading-instance --quiet --command="mkdir -p ~/$PROJECT_FOLDER"
-gcloud compute scp .env gcloud_vm_deploy.sh docker-compose-prod.yml ci-cd-pipeline@coffe-trading-instance:~/$PROJECT_FOLDER --quiet
-gcloud compute ssh ci-cd-pipeline@coffe-trading-instance --quiet \
+#gcloud compute instances start coffee-trading-instance
+gcloud compute ssh ci-cd-pipeline@coffee-trading-instance --quiet --command="mkdir -p ~/$PROJECT_FOLDER"
+gcloud compute scp .env gcloud_vm_deploy.sh docker-compose-prod.yml ci-cd-pipeline@coffee-trading-instance:~/$PROJECT_FOLDER --quiet
+gcloud compute ssh ci-cd-pipeline@coffee-trading-instance --quiet \
   --command="chmod +x ~/$PROJECT_FOLDER/gcloud_vm_deploy.sh && sh ~/$PROJECT_FOLDER/gcloud_vm_deploy.sh $GCLOUD_PULL_USERNAME $GCLOUD_PULL_PASSWORD"
