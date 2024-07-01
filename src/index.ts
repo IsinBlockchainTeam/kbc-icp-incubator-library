0a3dddd69de70f3a35107e09acfd097dd5b9c737
Luca Giussani <lucagiussani.lg@gmail.com>
2024-07-01 18:24:31 +0200

    change typescript configuration in order to have built code in commonjs (avoid "export" error)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { ExternalProvider } from '@ethersproject/providers';
+export type { ExternalProvider } from '@ethersproject/providers';
 export { Signer } from 'ethers';
 export { SignerUtils } from './utils/SignerUtils';
 
@@ -19,32 +19,32 @@ export { AssetOperationType } from './types/AssetOperationType';
 export { AssetOperationDriver } from './drivers/AssetOperationDriver';
 export { AssetOperationService } from './services/AssetOperationService';
 
-export { Trade } from './entities/Trade';
-export { Line } from './entities/Trade';
-export { LineRequest } from './entities/Trade';
-export { OrderLineRequest } from './entities/OrderTrade';
-export { BasicTrade } from './entities/BasicTrade';
-export { OrderTrade } from './entities/OrderTrade';
-export { OrderTradeMetadata } from './entities/OrderTrade';
-export { OrderLine } from './entities/OrderTrade';
-export { OrderLinePrice } from './entities/OrderTrade';
+export { Trade, Line, LineRequest } from './entities/Trade';
+export { BasicTrade, type BasicTradeMetadata } from './entities/BasicTrade';
+export {
+    OrderTrade,
+    OrderLine,
+    OrderLinePrice,
+    OrderLineRequest,
+    type OrderTradeMetadata
+} from './entities/OrderTrade';
 export { OrderStatus } from './types/OrderStatus';
 export { NegotiationStatus } from './types/NegotiationStatus';
 export { TradeType } from './types/TradeType';
 export { TradeManagerDriver } from './drivers/TradeManagerDriver';
 export { TradeManagerService } from './services/TradeManagerService';
-export { TradeManagerServiceArgs } from './services/TradeManagerService';
+export type { TradeManagerServiceArgs } from './services/TradeManagerService';
 export { TradeDriver } from './drivers/TradeDriver';
 export { TradeService } from './services/TradeService';
 export { BasicTradeDriver } from './drivers/BasicTradeDriver';
 export { BasicTradeService } from './services/BasicTradeService';
 export { OrderTradeDriver } from './drivers/OrderTradeDriver';
 export { OrderTradeService } from './services/OrderTradeService';
-export { IConcreteTradeService } from './services/IConcreteTradeService';
-export { URLStructure } from './types/URLStructure';
+export type { IConcreteTradeService } from './services/IConcreteTradeService';
+export type { URLStructure } from './types/URLStructure';
 
 export { DocumentInfo, DocumentType } from './entities/DocumentInfo';
-export { Document, DocumentStatus, TransactionLine } from './entities/Document';
+export { Document, DocumentStatus, type TransactionLine } from './entities/Document';
 export { DocumentDriver } from './drivers/DocumentDriver';
 export { DocumentService } from './services/DocumentService';
 
@@ -53,19 +53,20 @@ export { OfferDriver } from './drivers/OfferDriver';
 export { OfferService } from './services/OfferService';
 
 export { Escrow } from './entities/Escrow';
+export { EscrowStatus } from './types/EscrowStatus';
 export { EscrowManagerDriver } from './drivers/EscrowManagerDriver';
 export { EscrowManagerService } from './services/EscrowManagerService';
 export { EscrowDriver } from './drivers/EscrowDriver';
 export { EscrowService } from './services/EscrowService';
 
-export { ISolidStorageMetadataDriver } from './drivers/ISolidStorageMetadataDriver';
-export { ISolidStorageDocumentDriver } from './drivers/ISolidStorageDocumentDriver';
-export { SolidMetadataDriver, SolidMetadataSpec } from './drivers/SolidMetadataDriver';
-export { SolidDocumentDriver, SolidDocumentSpec } from './drivers/SolidDocumentDriver';
+export type { ISolidStorageMetadataDriver } from './drivers/ISolidStorageMetadataDriver';
+export type { ISolidStorageDocumentDriver } from './drivers/ISolidStorageDocumentDriver';
+export { SolidMetadataDriver, type SolidMetadataSpec } from './drivers/SolidMetadataDriver';
+export { SolidDocumentDriver, type SolidDocumentSpec } from './drivers/SolidDocumentDriver';
 
 export { ICPFileDriver } from './drivers/ICPFileDriver';
 
-export { GraphService, GraphData } from './services/GraphService';
+export { GraphService, type GraphData } from './services/GraphService';
 
 export { FileHelpers } from './utils/fileHelpers';
 export { serial } from './utils/utils';
