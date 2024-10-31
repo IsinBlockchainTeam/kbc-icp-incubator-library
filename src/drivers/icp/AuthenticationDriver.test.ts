fd40c4699390fb665688f1a86103477131966968
Lorenzo Ronzani <lorenzo.ronzani00@gmail.com>
2024-10-31 15:44:05 +0100

    test 🪖:  Resolved canister id issues

--- src/drivers/icp/AuthenticationDriver.test.ts
+++ src/drivers/icp/AuthenticationDriver.test.ts
@@ -4,7 +4,6 @@ import { ProductCategoryDriver } from './ProductCategoryDriver';
 import { SiweIdentityProvider } from './SiweIdentityProvider';
 import { computeRoleProof } from './proof';
 import { AuthenticationDriver } from './AuthenticationDriver';
-import { ShipmentDriver } from './ShipmentDriver';
 
 const USER1_PRIVATE_KEY = '0c7e66e74f6666b514cc73ee2b7ffc518951cf1ca5719d6820459c4e134f2264';
 const COMPANY1_PRIVATE_KEY = '538d7d8aec31a0a83f12461b1237ce6b00d8efc1d8b1c73566c05f63ed5e6d02';
@@ -16,8 +15,8 @@ const DELEGATE_CREDENTIAL_ID_HASH =
 const DELEGATOR_CREDENTIAL_ID_HASH =
     '0xf19b6aebcdaba2222d3f2c818ff1ecda71c7ed93c3e0f958241787663b58bc4b';
 // const ETH_PRIVATE_KEY = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80';
-const SIWE_CANISTER_ID = 'by6od-j4aaa-aaaaa-qaadq-cai';
-const ENTITY_MANAGER_CANISTER_ID = 'bw4dl-smaaa-aaaaa-qaacq-cai';
+const SIWE_CANISTER_ID = 'be2us-64aaa-aaaaa-qaabq-cai';
+const ENTITY_MANAGER_CANISTER_ID = 'bkyz2-fmaaa-aaaaa-qaaaq-cai';
 
 type Utils = {
     userWallet: Wallet;
