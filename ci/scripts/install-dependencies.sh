1dd106863110b5cded0bb1d17c2ce23f66f028b5
Lorenzo Ronzani <lorenzo.ronzani00@gmail.com>
2024-12-05 14:43:07 +0100

    wip 🚧:  Trying

--- ci/scripts/install-dependencies.sh
+++ ci/scripts/install-dependencies.sh
@@ -10,6 +10,9 @@ sh -ci "$(curl -fsSL https://internetcomputer.org/install.sh)"
 # Add cargo and dfx to PATH
 export PATH=$PATH:/root/.local/share/dfx/bin:/root/.cargo/bin
 
+# Add wasm target
+rustup target add wasm32-unknown-unknown
+
 # Verify installation
 cargo --version
 dfx --version
