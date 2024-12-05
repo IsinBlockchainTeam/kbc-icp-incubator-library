31368889aca167f23e9f4efb8ec6c15cc63ec4e2
Lorenzo Ronzani <lorenzo.ronzani00@gmail.com>
2024-12-05 11:44:46 +0100

    wip 🚧:  Trying

--- ci/scripts/install-dfx.sh
+++ ci/scripts/install-dfx.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# Install dfx
 export DFXVM_INIT_YES=true
 sh -ci "$(curl -fsSL https://internetcomputer.org/install.sh)"
+
+# Add dfx to PATH
 export PATH=$PATH:/root/.local/share/dfx/bin
+
+# Verify installation
 dfx --version
---
+++ icp/scripts/build-canisters.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+echo "BASE_DIR: $BASE_DIR"
+
+function generate_declarations() {
+  dfx canister create --all
+  dfx build --all
+  dfx generate
+}
+
+# Start dfx network
+dfx start --clean --background
+
+# Build Rust canisters
+cd "$BASE_DIR/rust-canisters"
+generate_declarations
+
+# Build ts canisters
+cd "$BASE_DIR/ts-canisters"
+set -o allexport
+source .env.ci
+set +o allexport
+generate_declarations
+npm run build-types
+
+# Stop dfx network
+dfx stop
