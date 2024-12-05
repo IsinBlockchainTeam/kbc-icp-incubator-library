23e77f9ab13b51b8c28b30cb1f8f38780131335a
Lorenzo Ronzani <lorenzo.ronzani00@gmail.com>
2024-12-05 10:11:22 +0100

    wip 🚧:  Trying

--- ci-scripts/install-dfx.sh
+++ ci-scripts/install-dfx.sh
@@ -2,6 +2,8 @@
 
 export DFXVM_INIT_YES=true
 sh -ci "$(curl -fsSL https://internetcomputer.org/install.sh)"
+echo 'export PATH=$PATH:/root/.local/share/dfx/bin' >> ~/.bashrc
+source ~/.bashrc
 echo 'export PATH=$PATH:/root/.local/share/dfx/bin' >> ~/.profile
 source ~/.profile
 dfx --version
