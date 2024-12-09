fe5026989ae9b5bfa5f9fca0660d8278ed4ef9fc
Lorenzo Ronzani <lorenzo.ronzani00@gmail.com>
2024-12-09 09:58:27 +0100

    wip 🚧:  Trying feat 🦄:  Imported correctly the identity

--- icp/scripts/import-identity-ic.sh
+++ icp/scripts/import-identity-ic.sh
@@ -3,12 +3,6 @@
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 echo "BASE_DIR: $BASE_DIR"
 
-PEM_FILE_PATH="$BASE_DIR/identity.pem"
-
-# Read pem file
-cat $CI_CD_ICP_IDENTITY_PEM > "$PEM_FILE_PATH"
-cat $PEM_FILE_PATH
-
 # Import identity
 # --storage-mode plaintext is used to store the identity in plaintext, useful for CI/CD
 dfx identity import ci-cd "$CI_CD_ICP_IDENTITY_PEM" --storage-mode plaintext
@@ -18,4 +12,3 @@ dfx identity use ci-cd
 # Set wallet
 dfx identity set-wallet $CI_CD_ICP_WALLET --ic
 dfx wallet balance --ic
-dfx wallet controllers --ic
